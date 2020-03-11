50cbe7ea25b72bb7d23b2b8beadec9ea0d6dd4c1
Andras Katona <andras.katona@atolldev.hu>
2020-03-11 09:15:27 +0100

    Initial commit

---
+++ src/test/java/com/elte/smartgym/SmartGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elte.smartgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
