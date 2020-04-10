50c76f5c00495af013b8dc938a80a3e89e62e91c
Andras Katona <andras.katona@atolldev.hu>
2020-04-10 13:59:21 +0200

    ExerciseController Unit Test Added Unit Tests for ExerciseController Edited CircleCI configuration

--- src/main/java/com/elte/smartgym/model/Exercise.java
+++ src/main/java/com/elte/smartgym/model/Exercise.java
@@ -1,12 +1,19 @@
 package com.elte.smartgym.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Exercise")
 public class Exercise {
 
--- src/test/java/com/elte/smartgym/SmartGymApplicationTests.java
+++ src/test/java/com/elte/smartgym/SmartGymApplicationTests.java
@@ -1,9 +1,7 @@
 package com.elte.smartgym;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SmartGymApplicationTests {
 
     @Test
